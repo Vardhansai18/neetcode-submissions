a4ca1ec9128cf1e1184f91fb9f0e3b8e2c277934
Vardhansai18 <102900233+Vardhansai18@users.noreply.github.com>
2026-07-06 21:51:25 +0530

    Add: longest-common-subsequence - submission-11

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-11.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int LCS( int i , int j , string &s1 , string &s2 , vector<vector<int>> &dp)
+    {
+        if( i == 0 || j == 0 ) return 0;
+        if( dp[i][j] != -1 ) return dp[i][j];
+        if( s1[i-1] == s2[j-1] ) 
+        {
+            return dp[i][j] = 1 + LCS( i - 1 , j - 1 , s1 , s2 , dp );
+        }
+        else
+        {
+            return dp[i][j] = max( LCS( i - 1 , j , s1 , s2 , dp ) , LCS( i , j - 1 , s1 , s2 , dp ) );
+        }
+    }
+    int longestCommonSubsequence(string s1, string s2) {
+
+        int m = s1.size() , n = s2.size();
+        // Memoization 
+        // vector<vector<int>> dp( m + 1 , vector<int>(n + 1 ,-1));
+        // return LCS( m , n , s1 , s2 , dp );
+
+        // TABULATION 
+        vector<vector<int>> dp( m + 1 , vector<int>(n + 1 ,0));
+        for( int i = 0 ; i < m ; i ++ ) dp[i][0] = 0;
+        for( int i = 0 ; i < n ; i ++ ) dp[0][i] = 0;
+
+        for( int i = 1 ; i <= m ; i++ )
+        {
+            for( int j = 1 ; j <= n ; j++ )
+            {
+                if( s1[i-1] == s2[j-1] ) 
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1] ;
+                }
+                else
+                {
+                    dp[i][j] = max( dp[i-1][j] , dp[i][j-1] );
+                }
+            }
+        } 
+        return dp[m][n];
+    }
+};
+
+/*
+
+s1 = cat 
+s2 = crabt
+
+  0 1 2 3  4
+0
+1
+2
+3
+
+
+
+
+cat
+ i
+
+ccat
+ j
+
+
+if( s1[i] == s2[j] ) 
+{
+    return 1 + solve( i + 1 , j + 1 , s1 , s2 );
+}
+else
+{
+    return solve( i + 1 , j + 1 , s1 , s2 );
+}
+*/
